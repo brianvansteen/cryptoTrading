92473e4d9fb828301616bd87e574a6e6016ee0d6
brianvansteen <bvansteen@gmail.com>
2022-05-29 13:03:35 -0400

    csv

---
+++ csvReader.h
@@ -0,0 +1,19 @@
+// specification file
+
+#pragma once
+#include "orderBookEntry.h"
+#include <vector>
+#include <string>
+
+
+class CSVReader
+{
+	public:
+		CSVReader();
+		static std::vector<OrderBookEntry> readCSV(std::string csvFile);
+
+	private:
+		static std::vector<std::string> tokenise(std::string csvLine, char separator);
+		static OrderBookEntry stringsToOBE(std::vector<std::string> strings);
+};
+
